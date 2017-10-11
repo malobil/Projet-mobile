da1fcadddc264b23ef2038965a0da0326c4e26f6
Seb <seb.gx@hotmail.fr>
2017-10-11 17:00:04 +0200

    Timer fonctionnel

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,17 +6,16 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour {
 
 	public float myWitchTimer;
-	private Text timerText;
+	public Text timerText;
 
 	// Use this for initialization
 	void Start () {
-		timerText=GetComponent <Text> ();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		myWitchTimer -= Time.deltaTime;
-		timerText.text = myWitchTimer.ToString ("f0");
-		print (myWitchTimer);
+		myWitchTimer-=Time.deltaTime;
+		timerText.text=myWitchTimer.ToString ("f0");
 	}
 }
