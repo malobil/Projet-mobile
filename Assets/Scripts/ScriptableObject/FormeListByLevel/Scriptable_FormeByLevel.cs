9ef3fa2c3a5b601b61e58864f240789ae81092b0
DESKTOP-92OCKSC\Kevin <ibanez13keke@gmail.com>
2017-10-11 17:03:24 +0200

    Pop utilise la touche P pour test

---
+++ Assets/Scripts/ScriptableObject/FormeListByLevel/Scriptable_FormeByLevel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="ListOfForm")]
+public class Scriptable_FormeByLevel : ScriptableObject 
+{
+	public Scriptable_Forme[] formList ;
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,26 +8,45 @@ public class Spawner : MonoBehaviour
 
 	public Scriptable_IngredientList associateList ;
 
-	public Image ingredientImage ;
+	public Scriptable_FormeByLevel associateFormList ;
 
-	private int randomNumber ;
+	public Image ingredientImage ;
+	public Image formImage ;
+
+	private int randomNumberIngredient ;
+	private int randomNumberForm ;
 
 
 	// Use this for initialization
 	void Start () 
 	{
-		PopIngredient() ;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		if(Input.GetKeyDown("p"))
+		{
+			PopIngredient() ;
+			PopForm() ;
+		}
+	}
+
+	void PopForm()
+	{
+		randomNumberForm = Random.Range(0, associateFormList.formList.Length) ;
+		formImage.sprite = associateFormList.formList[randomNumberForm].formeImage ;
 	}
 
 	void PopIngredient()
 	{
-		randomNumber = Random.Range(0, associateList.itemList.Length) ;
-		ingredientImage.sprite = associateList.itemList[randomNumber].ingredientImage ;
+		randomNumberIngredient = Random.Range(0, associateList.itemList.Length) ;
+		ingredientImage.sprite = associateList.itemList[randomNumberIngredient].ingredientImage ;
+	}
+
+	public void Test()
+	{
+		Debug.Log("Click") ;
 	}
 }
