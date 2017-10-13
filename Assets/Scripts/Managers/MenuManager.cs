1b2838a83ae2a8797f468d214f29974d83b8b8ac
Seb <seb.gx@hotmail.fr>
2017-10-13 08:57:45 +0200

    SHAME kevin

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public string MainScene;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void QuitMenuAccueil ()
+   	{
+        Application.Quit();
+        Debug.Log("quit game");
+    }
+
+	public void PlayMenuAccueil ()
+   	{
+        SceneManager.LoadScene (MainScene);
+   	}
+
+}
