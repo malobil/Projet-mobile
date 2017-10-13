f85b63682599815f6124d4c00f109f831e3db5d3
DESKTOP-92OCKSC\Kevin <ibanez13keke@gmail.com>
2017-10-13 11:56:18 +0200

    Recette 0.5.0

--- Assets/Scripts/Recettes/RecetteManager.cs
+++ Assets/Scripts/Recettes/RecetteManager.cs
@@ -4,13 +4,51 @@ using UnityEngine;
 
 public class RecetteManager : MonoBehaviour {
 
+	public int maximumIngredient = 2 ;
+	public List<GameObject> listOfIngredient = new List<GameObject>() ;
+
+	private static RecetteManager instance ;
+    public static RecetteManager Instance () 
+    {
+        return instance;
+    }
+
+void Awake ()
+    {
+        if (instance != null)
+        {
+            Destroy (gameObject);
+        }
+
+        else 
+
+        {
+            instance = this;
+        }
+    }
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
+	}
+
+	public void AddIngredient(GameObject ingredientToAdd)
+	{
+		if(listOfIngredient.Count < maximumIngredient)
+		{
+			listOfIngredient.Add(ingredientToAdd) ;
+			LevelManager.Instance().RetireObjectToList(ingredientToAdd) ;
+		}
+		else
+		{
+			ingredientToAdd.GetComponent<Formes_Et_Ingredients>().AddPoint() ;
+		}
 	}
 }
